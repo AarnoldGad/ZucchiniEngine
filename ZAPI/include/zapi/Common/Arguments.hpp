13716c8769bb06cb3fc0f636f7fb155bac762462
- The Aarnold - <mane.imation9@gmail.com>
2021-10-12 02:01:54 +0200

    Add program arguments interface

---
+++ ZAPI/include/zapi/Common/Arguments.hpp
@@ -0,0 +1,51 @@
+/**
+ * Arguments.hpp
+ * 12 Oct 2021
+ * Gaétan "The Aarnold" Jalin
+ *
+ * Copyright (C) 2020-2021 Gaétan Jalin
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *    1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ *    2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ *    3. This notice may not be removed or altered from any source distribution.
+ **/
+#ifndef ZE_ARGUMENTS_HPP
+#define ZE_ARGUMENTS_HPP
+
+#include "zapi/defines.hpp"
+
+#include <string>
+#include <vector>
+#include <optional>
+
+namespace ze
+{
+   class ZE_API Arguments final
+   {
+   public:
+      static void Set(int argc, char* argv[]);
+      
+      static std::string Get(size_t index);
+      static size_t GetCount() noexcept;
+      static std::optional<std::string> Find(std::string const& value); // TODO Get index if found
+
+   private:
+      static std::vector<std::string> s_arguments;
+   };
+}
+
+#endif /* ZE_ARGUMENTS_HPP */
