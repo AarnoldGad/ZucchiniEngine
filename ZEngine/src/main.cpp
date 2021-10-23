7bc47e38e151f749484883547a876a2b3a2bc2aa
- The Aarnold - <manelgad@gmail.com>
2021-10-23 17:27:56 +0200

    Update core

--- ZEngine/src/main.cpp
+++ ZEngine/src/main.cpp
@@ -1,17 +1,43 @@
 #include <zengine/zengine.hpp>
 
+#include <optional>
+#include <filesystem>
+
+std::optional<std::filesystem::path> findAppFile(ze::Arguments const& args)
+{
+   ze::Arguments::Argument const* appLibArg = args.find("--app-lib"); // Find corresponding argument
+
+   if (appLibArg && appLibArg->next) // Check argument validity
+      return appLibArg->next->value;
+
+   return std::nullopt;
+}
+
 int main(int argc, char* argv[])
 {
    ze::Arguments args(argc, argv);
 
-   ZE_LOG_DEBUG("Hello Engine");
+   ZE_LOG_DEBUG("Hello Engine !");
+   auto appLibPath = findAppFile(args); // Get library location
 
-   ze::Core::Initialise();
+   if (appLibPath)
+   {
+      ze::SharedObject appLib(appLibPath.value()); // Load app library
+      auto appEntryPoint = (void (*)(ze::Arguments const&)) appLib.getSymbol("Main"); // Get application's "Main" entry point function
 
-   ze::SharedObject app("../../App/bin/app-d.dll");
-   auto mainFn = (void (*)(ze::Arguments const&)) app.getSymbol("Main");
-   (*mainFn)(args);
+      if (appLib && appEntryPoint)
+      {
+         ze::Core::Initialise();
 
-   ze::Core::Terminate();
+         (*appEntryPoint)(args); // Run application
+
+         ze::Core::Terminate();
+      }
+      else
+         ZE_LOG_ERROR("Fail to catch application entry point !");
+   }
+   else
+      ZE_LOG_ERROR("No application library specified !");
+
    return 0;
 }
