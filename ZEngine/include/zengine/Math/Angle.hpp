53342473a59147ff545180b89bbf4116e038d33b
- The Aarnold - <mane.imation9@gmail.com>
2021-10-24 00:52:11 +0200

    Add Angle class

---
+++ ZEngine/include/zengine/Math/Angle.hpp
@@ -0,0 +1,99 @@
+/**
+ * Angle.hpp
+ * 26 Sep 2021
+ * Gaétan "The Aarnold" Jalin
+ *
+ * Copyright (C) 2020-2021 Gaétan Jalin
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *    1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ *    2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ *    3. This notice may not be removed or altered from any source distribution.
+ **/
+#ifndef ZE_ANGLE_HPP
+#define ZE_ANGLE_HPP
+
+#include "zengine/defines.hpp"
+
+#include <cmath>
+
+namespace ze
+{
+   class Angle;
+
+   Angle degrees(float deg) noexcept;
+   Angle radians(float rad) noexcept;
+   Angle arcseconds(float seconds) noexcept;
+   Angle arcminutes(float minutes) noexcept;
+
+   namespace literals
+   {
+      Angle operator""_deg(long double deg) noexcept;
+      Angle operator""_rad(long double rad) noexcept;
+      Angle operator""_asec(long double seconds) noexcept;
+      Angle operator""_amin(long double seconds) noexcept;
+   }
+
+   class ZE_API Angle
+   {
+   public:
+      float asDegrees() const noexcept;
+      float asRadians() const noexcept;
+      float asArcSeconds() const noexcept;
+      float asArcMinutes() const noexcept;
+
+      Angle operator-() const noexcept;
+
+      Angle& operator+=(Angle const& other) noexcept;
+      Angle& operator-=(Angle const& other) noexcept;
+      Angle& operator*=(Angle const& other) noexcept;
+      Angle& operator*=(float factor) noexcept;
+      Angle& operator/=(Angle const& other) noexcept;
+      Angle& operator/=(float factor) noexcept;
+      Angle& operator%=(Angle const& other) noexcept;
+      Angle& operator%=(float factor) noexcept;
+
+      Angle& operator+(Angle const& other) noexcept;
+      Angle& operator-(Angle const& other) noexcept;
+      Angle& operator*(Angle const& other) noexcept;
+      Angle& operator*(float factor) noexcept;
+      Angle& operator/(Angle const& other) noexcept;
+      Angle& operator/(float factor) noexcept;
+      Angle& operator%(Angle const& other) noexcept;
+      Angle& operator%(float factor) noexcept;
+
+      bool operator==(Angle const& other) const noexcept;
+      bool operator!=(Angle const& other) const noexcept;
+      bool operator<=(Angle const& other) const noexcept;
+      bool operator>=(Angle const& other) const noexcept;
+      bool operator<(Angle const& other) const noexcept;
+      bool operator>(Angle const& other) const noexcept;
+
+   private:
+      explicit Angle(float deg);
+
+      float m_degrees;
+
+      friend Angle degrees(float) noexcept;
+      friend Angle radians(float) noexcept;
+      friend Angle arcseconds(float) noexcept;
+      friend Angle arcminutes(float) noexcept;
+   };
+}
+
+#include "Angle.inl"
+
+#endif /* ZE_ANGLE_HPP */
