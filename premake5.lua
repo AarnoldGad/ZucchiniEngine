a011687fa7fef01fb45f479b3b2da3b68a4d5edb
- The Aarnold - <mane.imation9@gmail.com>
2022-02-03 00:15:34 +0100

    Deprecate premake

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+workspace "ZucchiniEngine"
+   architecture "x86_64"
+   configurations { "Debug", "Release" }
+
+   filter "system:linux or macosx"
+      sysincludedirs {
+         "/usr/include",
+         "/usr/local/include"
+      }
+      syslibdirs {
+         "/usr/lib",
+         "/usr/local/lib"
+      }
+
+   filter "configurations:Debug"
+      defines "_DEBUG"
+      runtime "Debug"
+      symbols "on"
+
+   filter "configurations:Release"
+      defines "NDEBUG"
+      runtime "Release"
+      optimize "on"
+
+include "ZEngine"
