ab71ad4c4fb660a44a5f3dc328aa37b0d9f3439a
- The Aarnold - <mane.imation9@gmail.com>
2022-03-12 15:55:26 +0100

    Extend logging macros

--- ZEngine/include/zengine/defines.hpp
+++ ZEngine/include/zengine/defines.hpp
@@ -25,11 +25,23 @@ typedef struct
 
 #define CURRENT_SOURCE_LOCATION SourceLocation{__FILENAME__, static_cast<unsigned int>(__LINE__), __func__ }
 
+#define ZE_INFO_OUTPUT     ::ze::Core::UseCoreLogger().info()
+#define ZE_DEBUG_OUTPUT    ::ze::Core::UseCoreLogger().debug()
+#define ZE_WARN_OUTPUT     ::ze::Core::UseCoreLogger().warn()
+#define ZE_ERROR_OUTPUT    ::ze::Core::UseCoreLogger().error()
+#define ZE_CRITICAL_OUTPUT ::ze::Core::UseCoreLogger().critical()
+
 #define ZE_LOG_INFO(...)     ::ze::Core::UseCoreLogger().info().logLine(__VA_ARGS__)
 #define ZE_LOG_DEBUG(...)    ::ze::Core::UseCoreLogger().debug().logLine(__VA_ARGS__)
 #define ZE_LOG_WARN(...)     ::ze::Core::UseCoreLogger().warn().logLine(__VA_ARGS__)
 #define ZE_LOG_ERROR(...)    ::ze::Core::UseCoreLogger().error().logLine(__VA_ARGS__)
 #define ZE_LOG_CRITICAL(...) ::ze::Core::UseCoreLogger().critical().logLine(__VA_ARGS__)
+
+#define APP_INFO_OUTPUT     ::ze::Core::UseAppLogger().info()
+#define APP_DEBUG_OUTPUT    ::ze::Core::UseAppLogger().debug()
+#define APP_WARN_OUTPUT     ::ze::Core::UseAppLogger().warn()
+#define APP_ERROR_OUTPUT    ::ze::Core::UseAppLogger().error()
+#define APP_CRITICAL_OUTPUT ::ze::Core::UseAppLogger().critical()
 
 #define APP_LOG_INFO(...)     ::ze::Core::UseAppLogger().info().logLine(__VA_ARGS__)
 #define APP_LOG_DEBUG(...)    ::ze::Core::UseAppLogger().debug().logLine(__VA_ARGS__)
